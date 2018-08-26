b18350bbb45e1ee955ce6216c1f9200169f20a1f
Marta <martadabrowka@gmail.com>
2018-08-26 17:52:05 +0100

    adds a form to log spendings

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View , Image} from 'react-native';
+import { StyleSheet, Text, View , Image } from 'react-native';
+import { FormLabel, FormInput } from 'react-native-elements';
 
 export default class App extends React.Component {
+  state = {
+    spendings: [],
+  }
   render() {
     let pic = {
       uri: "http://themellorpractice.co.uk/wp-content/uploads/2014/06/cash-money-pounds.jpg"
     }
     return (
       <View style={styles.container}>
-        <Text>Budgeter</Text>
+        <Text>Coin Jar</Text>
         <Image source={pic} style={{width: 193, height: 110}}/>
+        <FormLabel>What did you buy?</FormLabel>
+        <FormInput
+          ref={input => this.input = input} />
+        <FormLabel>How much did you spend?</FormLabel>
+        <FormInput 
+          ref={input => this.input = input}/>
       </View>
     );
   }
